80daf83f65b6f25dde904e5ccd9c153f3ad941a6
Priyansh Yawalkar <78356829+PriyanshYawalkar@users.noreply.github.com>
2022-10-09 19:19:12 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+console.log("Welcome to my website")
+
+function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav"
+    }
+}
+
+
+function homefunction() {
+    getElementById(home)
+}
+
+
